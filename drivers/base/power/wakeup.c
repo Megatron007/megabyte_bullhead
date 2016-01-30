21b66e7d01ba14944a7b12107482e842ebdacab9
flar2 <asegaert@gmail.com>
2016-01-30 00:58:45 -0500

    block IPA_WS wakelock

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -30,6 +30,9 @@ module_param(enable_bluedroid_timer_ws, bool, 0644);
 static bool enable_ipa_ws = true;
 module_param(enable_ipa_ws, bool, 0644);
 
+static bool enable_ipa_ws = true;
+module_param(enable_ipa_ws, bool, 0644);
+
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
